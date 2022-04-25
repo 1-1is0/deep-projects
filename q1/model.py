9fd7141855d7c828069781d2145f9cab08b75764
1-1is0 <amirftma@gmail.com>
2022-04-25 07:58:00 +0430

    q1

--- q1/model.py
+++ q1/model.py
@@ -1,4 +1,5 @@
 import torch
+import os
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -38,3 +39,91 @@ class Net(nn.Module):
         x = self.deconv(x)
         return x
 
+def train_model(
+        data_loader,
+        dataset_size,
+        model,
+        criterion,
+        optimizer,
+        scheduler,
+        epochs,
+        device,
+        path="/content/drive/MyDrive/data/state",
+):
+    state_file_name = f"{path}/state-{model._get_name()}.pth"
+    state_res = {}
+    state_epoch = 0
+    if os.path.exists(state_file_name):
+        state = torch.load(state_file_name)
+        model.load_state_dict(state["state_dict"])
+        optimizer.load_state_dict(state["optimizer"])
+        scheduler.load_state_dict(state["scheduler"])
+        state_res = state["res"]
+        state_epoch = state["epoch"]
+
+    res = {
+        "epoch_loss_train": state_res.get("epoch_loss_train", []),
+        "loss_history_train": state_res.get("loss_history_train", []),
+        "lr": state_res.get("lr", []),
+        "epoch_loss_val": state_res.get("epoch_loss_val", []),
+        "loss_history_val": state_res.get("loss_history_val", []),
+    }
+    # remaining epochs
+    epochs = epochs - state_epoch
+    # loss_history = []
+    # epochs_loss = []
+    for epoch in range(epochs):  # loop over the dataset multiple times
+
+        for phase in ["train", "val"]:
+            if phase == "train":
+                model.train(True)  # Set model to training mode
+            else:
+                model.train(False)  # Set model to evaluate mode
+
+            running_loss = 0.0
+            log = 0
+
+            for i, data in enumerate(data_loader[phase], 0):
+                # get the inputs; data is a list of [inputs, labels]
+                # name, image, fix_map = data
+                image = data['image'].to(device)
+                fix_map = data['fix_map'].to(device)
+                optimizer.zero_grad()
+                # forward + backward + optimize
+                outputs = model(image)
+                loss = criterion(outputs, fix_map)
+                if phase == "train":
+                    loss.backward()
+                    optimizer.step()
+                # print statistics
+                # print("data shape", data["image"].shape)
+                now_batch_size = data["image"].size()[0]
+                # print("len dataloader", dataset_size[phase], "now batch size", now_batch_size)
+
+                running_loss += loss.item() * now_batch_size
+                log += 1
+                if len(data_loader) / 5 == log:
+                    print(". ", end="")
+                    log = 0
+            # add loss at the end of each iteration
+            # loss_history.append(running_loss)
+            if phase == "train":
+                res["lr"].append(optimizer.param_groups[0]["lr"])
+                scheduler.step()
+
+            print("epoch", epoch, running_loss / len(data_loader[phase]))
+
+            epoch_loss = running_loss / dataset_size[phase]
+            res[f"epoch_loss_{phase}"].append(epoch_loss)
+            res[f"loss_history_{phase}"].append(running_loss)
+
+        state = {
+            "epoch": epoch,
+            "state_dict": model.state_dict(),
+            "optimizer": optimizer.state_dict(),
+            "scheduler": scheduler.state_dict(),
+            "res": res,
+        }
+        torch.save(state, state_file_name)
+    print("Finished Training")
+    return res
