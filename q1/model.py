3d1d1ba9f29790877aa6ceaedac1acc8df92421d
1-1is0 <amirftma@gmail.com>
2022-04-27 13:43:34 +0430

    update wieghts

--- q1/model.py
+++ q1/model.py
@@ -3,26 +3,30 @@ import os
 import torch.nn as nn
 import torch.nn.functional as F
 
+# todo inti model wietgh'
+# batcj 2
+# cv2 to o piil miage
+
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         # 1 input image channel, 6 output channels, 3x3 square convolution
         # kernel
-        self.conv1 = nn.Conv2d(3, 96, 7, stride=1, padding=3)
-        self.local_response_norm = nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75)
-        self.max_pool1 = nn.MaxPool2d((3,3), stride=2)
-        self.conv2 = nn.Conv2d(96, 256, 5, stride=1, padding=2)
-        self.max_pool2 = nn.MaxPool2d((3,3), stride=2, padding=0)
-        self.conv3 = nn.Conv2d(256, 512, 3, stride=1, padding=1)
-        self.conv4 = nn.Conv2d(512, 512, 5, stride=1, padding=2)
-        self.conv5 = nn.Conv2d(512, 512, 5, stride=1, padding=2)
-        self.conv6 = nn.Conv2d(512, 256, 7, stride=1, padding=3)
-        self.conv7 = nn.Conv2d(256, 128, 11, stride=1, padding=5)
-        self.conv8 = nn.Conv2d(128, 32, 11, stride=1, padding=5)
-        # self.conv9 = nn.Conv2d(32, 1, 13, stride=1, padding=6)
-        # I remove padding 2 from Conv2d Transpose because the output dimension was not right
-        self.deconv = nn.ConvTranspose2d(32, 1, 8, stride=4, bias=False)
 
+        self.conv1 = nn.Conv2d(3, 48, 7, stride=1, padding=3)
+        self.local_response_norm = nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75)
+        self.max_pool1 = nn.MaxPool2d((3, 3), stride=2)
+        self.conv2 = nn.Conv2d(48, 128, 5, stride=1, padding=2)
+        self.max_pool2 = nn.MaxPool2d((3, 3), stride=2, padding=0)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(256, 256, kernel_size=5, stride=1, padding=2)
+        self.conv5 = nn.Conv2d(256, 256, kernel_size=5, stride=1, padding=2)
+        self.conv6 = nn.Conv2d(256, 128, kernel_size=7, stride=1, padding=3)
+        self.conv7 = nn.Conv2d(128, 64, kernel_size=11, stride=1, padding=5)
+        self.conv8 = nn.Conv2d(64, 16, kernel_size=11, stride=1, padding=5)
+        self.conv9 = nn.Conv2d(16, 1, kernel_size=13, stride=1, padding=6)
+        self.deconv = nn.ConvTranspose2d(1, 1, kernel_size=8, stride=4, bias=False)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -36,8 +40,21 @@ class Net(nn.Module):
         x = F.relu(self.conv6(x))
         x = F.relu(self.conv7(x))
         x = F.relu(self.conv8(x))
+        x = F.relu(self.conv9(x))
         x = self.deconv(x)
         return x
+
+    def initialize_weights(self):
+        # https://www.askpython.com/python-modules/initialize-model-weights-pytorch
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.ConvTranspose2d):
+                nn.init.normal_(m.weight, mean=0, std=0.01)
+                if m.bias is not None:
+                    m.bias.data.zero_()
 
 def train_model(
         data_loader,
@@ -81,17 +98,18 @@ def train_model(
                 model.train(False)  # Set model to evaluate mode
 
             running_loss = 0.0
-            log = 0
 
             for i, data in enumerate(data_loader[phase], 0):
                 # get the inputs; data is a list of [inputs, labels]
                 # name, image, fix_map = data
                 image = data['image'].to(device)
                 fix_map = data['fix_map'].to(device)
-                optimizer.zero_grad()
                 # forward + backward + optimize
                 outputs = model(image)
                 loss = criterion(outputs, fix_map)
+
+                optimizer.zero_grad()
+
                 if phase == "train":
                     loss.backward()
                     optimizer.step()
@@ -99,18 +117,14 @@ def train_model(
                 # print("data shape", data["image"].shape)
                 now_batch_size = data["image"].size()[0]
                 # print("len dataloader", dataset_size[phase], "now batch size", now_batch_size)
-
                 running_loss += loss.item() * now_batch_size
-                log += 1
-                if len(data_loader) / 5 == log:
-                    print(". ", end="")
-                    log = 0
             # add loss at the end of each iteration
             # loss_history.append(running_loss)
             if phase == "train":
                 res["lr"].append(optimizer.param_groups[0]["lr"])
                 scheduler.step()
 
+            print(phase, len(data_loader[phase]))
             print("epoch", epoch, running_loss / len(data_loader[phase]))
 
             epoch_loss = running_loss / dataset_size[phase]
