8ce0d26e9190cf3ffff236562cf6ef6a77c593e4
1-1is0 <amirftma@gmail.com>
2022-04-28 18:11:29 +0430

    sutff

--- q2/dataloader.py
+++ q2/dataloader.py
@@ -134,7 +134,7 @@ def get_datasets(train_df, test_df):
                 transforms.ToTensor(),
                 transforms.Normalize(
                     mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-                # ),
+                ),
             ]
         )
     )
